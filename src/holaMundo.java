a352f0ecb6e62015830ba27f4954b61b3435eea8
marinaaparicio <marinaapariciosantos@gmail.com>
2022-09-26 13:31:29 +0200

    cambio

--- src/holaMundo.java
+++ src/holaMundo.java
@@ -3,7 +3,7 @@ public class holaMundo {
 	
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "Hola mundo" );
+	        System.out.println( "Hola mundo soy Marina Aparicio Santos" );
 	    }
 
 }
